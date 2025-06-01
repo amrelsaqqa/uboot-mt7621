e9c6d63ee1b28779d803ddb610778173ba08e988
amrelsaqqa <amrelsaqqa@hotmail.com>
2025-06-01 20:24:24 +0300

    Update test_util.py, python3

--- tools/patman/test_util.py
+++ tools/patman/test_util.py
@@ -10,70 +10,68 @@ import sys
 
 import command
 
-try:
-  from StringIO import StringIO
-except ImportError:
-  from io import StringIO
+from io import StringIO  # Python 3 only
 
 
 def RunTestCoverage(prog, filter_fname, exclude_list, build_dir, required=None):
     """Run tests and check that we get 100% coverage
 
     Args:
-        prog: Program to run (with be passed a '-t' argument to run tests
-        filter_fname: Normally all *.py files in the program's directory will
-            be included. If this is not None, then it is used to filter the
-            list so that only filenames that don't contain filter_fname are
-            included.
-        exclude_list: List of file patterns to exclude from the coverage
-            calculation
+        prog: Program to run (will be passed a '-t' argument to run tests)
+        filter_fname: If not None, only filenames containing this string are included
+        exclude_list: List of file patterns to exclude from coverage calculation
         build_dir: Build directory, used to locate libfdt.py
-        required: List of modules which must be in the coverage report
+        required: Set of module names that must be in the coverage report
 
     Raises:
-        ValueError if the code coverage is not 100%
+        ValueError: If the code coverage is not 100% or required modules are missing
     """
-    # This uses the build output from sandbox_spl to get _libfdt.so
     path = os.path.dirname(prog)
+    glob_list = []
     if filter_fname:
-        glob_list = glob.glob(os.path.join(path, '*.py'))
-        glob_list = [fname for fname in glob_list if filter_fname in fname]
-    else:
-        glob_list = []
+        all_files = glob.glob(os.path.join(path, '*.py'))
+        glob_list = [fname for fname in all_files if filter_fname in fname]
+
     glob_list += exclude_list
     glob_list += ['*libfdt.py', '*site-packages*']
-    cmd = ('PYTHONPATH=$PYTHONPATH:%s/sandbox_spl/tools python-coverage run '
-           '--omit "%s" %s -t' % (build_dir, ','.join(glob_list), prog))
+
+    omit_list = ','.join(glob_list)
+    cmd = (
+        f'PYTHONPATH=$PYTHONPATH:{build_dir}/sandbox_spl/tools '
+        f'python3 -m coverage run --omit "{omit_list}" {prog} -t'
+    )
     os.system(cmd)
-    stdout = command.Output('python-coverage', 'report')
+
+    stdout = command.Output('python3 -m coverage', 'report')
     lines = stdout.splitlines()
+    ok = True
+
     if required:
-        # Convert '/path/to/name.py' just the module name 'name'
-        test_set = set([os.path.splitext(os.path.basename(line.split()[0]))[0]
-                        for line in lines if '/etype/' in line])
-        missing_list = required
-        missing_list.difference_update(test_set)
+        # Convert '/path/to/name.py' to 'name'
+        test_set = {
+            os.path.splitext(os.path.basename(line.split()[0]))[0]
+            for line in lines if '/etype/' in line
+        }
+        missing_list = required - test_set
         if missing_list:
-            print 'Missing tests for %s' % (', '.join(missing_list))
-            print stdout
+            print(f"Missing tests for {', '.join(sorted(missing_list))}")
+            print(stdout)
             ok = False
 
-    coverage = lines[-1].split(' ')[-1]
-    ok = True
-    print coverage
+    coverage = lines[-1].split()[-1]
+    print(coverage)
     if coverage != '100%':
-        print stdout
-        print ("Type 'python-coverage html' to get a report in "
-               'htmlcov/index.html')
-        print 'Coverage error: %s, but should be 100%%' % coverage
+        print(stdout)
+        print("Type 'python3 -m coverage html' to get a report in htmlcov/index.html")
+        print(f'Coverage error: {coverage}, but should be 100%')
         ok = False
+
     if not ok:
         raise ValueError('Test coverage failure')
 
 
 # Use this to suppress stdout/stderr output:
 # with capture_sys_output() as (stdout, stderr)
-#   ...do something...
 @contextmanager
 def capture_sys_output():
     capture_out, capture_err = StringIO(), StringIO()
