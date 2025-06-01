a778a4cca38af41b6527e18b6640cac0771de1db
amrelsaqqa <amrelsaqqa@hotmail.com>
2025-06-01 19:12:09 +0300

    Update binman.py, python3 syntax

--- tools/binman/binman.py
+++ tools/binman/binman.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 # SPDX-License-Identifier: GPL-2.0+
 
 # Copyright (c) 2016 Google, Inc
@@ -28,14 +28,9 @@ import command
 import control
 import test_util
 
-def RunTests(debug, args):
-    """Run the functional tests and any embedded doctests
 
-    Args:
-        debug: True to enable debugging, which shows a full stack trace on error
-        args: List of positional args provided to binman. This can hold a test
-            name to execute (as in 'binman -t testSections', for example)
-    """
+def RunTests(debug, args):
+    """Run the functional tests and any embedded doctests"""
     import elf_test
     import entry_test
     import fdt_test
@@ -53,11 +48,14 @@ def RunTests(debug, args):
     if debug:
         sys.argv.append('-D')
 
-    # Run the entry tests first ,since these need to be the first to import the
-    # 'entry' module.
-    test_name = args and args[0] or None
-    for module in (entry_test.TestEntry, ftest.TestFunctional, fdt_test.TestFdt,
-                   elf_test.TestElf, image_test.TestImage):
+    test_name = args[0] if args else None
+    for module in (
+        entry_test.TestEntry,
+        ftest.TestFunctional,
+        fdt_test.TestFdt,
+        elf_test.TestElf,
+        image_test.TestImage,
+    ):
         if test_name:
             try:
                 suite = unittest.TestLoader().loadTestsFromName(test_name, module)
@@ -67,47 +65,48 @@ def RunTests(debug, args):
             suite = unittest.TestLoader().loadTestsFromTestCase(module)
         suite.run(result)
 
-    print result
+    print(result)
     for test, err in result.errors:
-        print test.id(), err
+        print(test.id(), err)
     for test, err in result.failures:
-        print err, result.failures
+        print(err, result.failures)
+
     if result.errors or result.failures:
-      print 'binman tests FAILED'
-      return 1
+        print('binman tests FAILED')
+        return 1
     return 0
 
-def GetEntryModules(include_testing=True):
-    """Get a set of entry class implementations
 
-    Returns:
-        Set of paths to entry class filenames
-    """
+def GetEntryModules(include_testing=True):
+    """Get a set of entry class implementations"""
     glob_list = glob.glob(os.path.join(our_path, 'etype/*.py'))
-    return set([os.path.splitext(os.path.basename(item))[0]
-                for item in glob_list
-                if include_testing or '_testing' not in item])
+    return {
+        os.path.splitext(os.path.basename(item))[0]
+        for item in glob_list
+        if include_testing or '_testing' not in item
+    }
+
 
 def RunTestCoverage():
     """Run the tests and check that we get 100% coverage"""
     glob_list = GetEntryModules(False)
-    all_set = set([os.path.splitext(os.path.basename(item))[0]
-                   for item in glob_list if '_testing' not in item])
-    test_util.RunTestCoverage('tools/binman/binman.py', None,
-            ['*test*', '*binman.py', 'tools/patman/*', 'tools/dtoc/*'],
-            options.build_dir, all_set)
+    all_set = {
+        os.path.splitext(os.path.basename(item))[0]
+        for item in glob_list
+        if '_testing' not in item
+    }
+    test_util.RunTestCoverage(
+        'tools/binman/binman.py',
+        None,
+        ['*test*', '*binman.py', 'tools/patman/*', 'tools/dtoc/*'],
+        options.build_dir,
+        all_set,
+    )
+
 
 def RunBinman(options, args):
-    """Main entry point to binman once arguments are parsed
-
-    Args:
-        options: Command-line options
-        args: Non-option arguments
-    """
+    """Main entry point to binman once arguments are parsed"""
     ret_code = 0
-
-    # For testing: This enables full exception traces.
-    #options.debug = True
 
     if not options.debug:
         sys.tracebacklimit = 0
@@ -125,15 +124,16 @@ def RunBinman(options, args):
         try:
             ret_code = control.Binman(options, args)
         except Exception as e:
-            print 'binman: %s' % e
+            print(f'binman: {e}')
             if options.debug:
-                print
+                print()
                 traceback.print_exc()
             ret_code = 1
+
     return ret_code
 
 
 if __name__ == "__main__":
-    (options, args) = cmdline.ParseArgs(sys.argv)
+    options, args = cmdline.ParseArgs(sys.argv)
     ret_code = RunBinman(options, args)
     sys.exit(ret_code)
